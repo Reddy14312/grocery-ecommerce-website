79de6da3b686fe76505851b26e8fcc1c2642e283
Reddy14312 <Chowdeshwar_reddy@outlook.com>
2025-11-10 14:13:44 +0530

    Publish application

---
+++ build/precache-manifest.d4664b69306b77f1656d9ef8007b5bf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d190c7a5767111dd2230991d7dffe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075c7e49e8cce7bcef1b",
+    "url": "/static/css/2.7f5860a0.chunk.css"
+  },
+  {
+    "revision": "cbb30f626ba86eeacc2a",
+    "url": "/static/css/main.be2cd11b.chunk.css"
+  },
+  {
+    "revision": "075c7e49e8cce7bcef1b",
+    "url": "/static/js/2.5616bfe2.chunk.js"
+  },
+  {
+    "revision": "1ef4bb249bd6a25060903e88f4959def",
+    "url": "/static/js/2.5616bfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb30f626ba86eeacc2a",
+    "url": "/static/js/main.cef54e1e.chunk.js"
+  },
+  {
+    "revision": "7e6a8db47024c1c6b2b3",
+    "url": "/static/js/runtime-main.7e0e0399.js"
+  },
+  {
+    "revision": "419b45388b591cb76de2a07891df90f2",
+    "url": "/static/media/notfound.419b4538.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
